48d565c2b9d604e76f9f1cc7f7695cd064de4a2c
Victor Gil <baskeboler@gmail.com>
2020-02-02 03:49:25 +0000

    max prime factor and pascal triangle

--- src/clojure_katas/max_prime_factor.clj
+++ src/clojure_katas/max_prime_factor.clj
@@ -9,5 +9,17 @@
    Iterative case:
     if number can be divided by div, then decrement number by the factor of div, loop
     if number cannot be divided by div, then increment div by 1, loop"
-    [number])
+  [number]
+  (let [number-sqrt (long (Math/sqrt number))]
+    (loop [n number 
+           d 2]
+      (cond 
+        (= n d) n 
+        (> d number-sqrt) n 
+        :else (recur (if (zero? (rem n d)) 
+                       (long (/ n d))
+                       n)
+                     (if-not (zero? (rem n d))
+                       (inc d)
+                       d))))))
 
--- src/clojure_katas/pascal_triangle.clj
+++ src/clojure_katas/pascal_triangle.clj
@@ -1,11 +1,36 @@
 (ns clojure-katas.pascal-triangle
   (:require [clojure-katas.core :as core]))
 
+
+(defn pascal1 [row col]
+  (cond
+    (or (zero? row)
+        (zero? col)
+        (= row col)) 1
+    :else (long 
+           (+ (pascal1 (dec row) (dec col))
+              (pascal1 (dec row) col)))))
+
 (core/defproblem compute
   "Use tree recursion to solve pascal triangle,
    it builds up a tree of vars and uneccesarily
    builds up a stack in memory, highly ineffective"
-  [row col])
+  [row col]
+  (pascal1 row col))
+
+(defn- pascal-row-mults [row]
+  (for [n (range 1 (inc row))] 
+    (/ (- (inc row) n)
+       n)))
+
+(defn- pascal-row [row]
+  (loop [res [1]
+         factors (pascal-row-mults row)]
+    (if (empty? factors)
+      res
+      (recur (conj res (* (last res) 
+                          (first factors)))
+             (rest factors)))))
 
 (core/defproblem compute-alt
   "Use iterative recursion to solve pascal triangle,
@@ -15,5 +40,12 @@
   stop building
   and return the current iterating value for requested
   position."
-  [row col])
+  [row col]
+  (cond 
+    (or (zero? row)
+        (zero? col)) 1
+    :else (->> (pascal-row row)
+               (drop col)
+               first)))
+         
 
